554ba9fd595086cdc5fe36dac6e0ee27042eb804
Bobby <bobby.tennant@icloud.com>
2026-05-26 07:55:53 +0100

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,51 @@
+document.body.insertAdjacentHTML("beforeend", `
+<footer class="footer" id="resources">
+  <div class="footer-grid">
+    <div class="footer-brand">
+      <img
+        src="https://mrtennant-music.github.io/musicliteracy/the-music-literacy-hub-logo.svg"
+        alt="The Music Literacy Hub"
+        class="brand-wordmark footer-wordmark"
+      />
+    </div>
+
+    <div>
+      <h4>About</h4>
+      <p>
+        Interactive apps for developing music literacy skills.
+        Find out more
+        <a class="text-link" href="#" data-open="aboutOverlay">here</a>.
+      </p>
+    </div>
+
+    <div>
+      <h4>Feedback</h4>
+      <p>
+        If you have any suggestions on how to improve the hub,
+        please click
+        <a
+          class="text-link"
+          href="https://forms.cloud.microsoft/e/vW5PPdW154"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>.
+      </p>
+    </div>
+
+    <div id="contact">
+      <h4>Contact</h4>
+      <p>
+        Robert Tennant<br />
+        Teacher of Music<br />
+        gw19tennantrobert1@glow.sch.uk
+      </p>
+    </div>
+  </div>
+
+  <div class="copyright">
+    The Music Literacy Hub · Version 1.0 · 2026
+  </div>
+</footer>
+`);
